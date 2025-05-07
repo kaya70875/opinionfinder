75780d78fe31dd218e302e9eb720ab95a5a46959
Ahmet Kaya <shortsforturkey@gmail.com>
2025-05-07 12:21:36 +0300

    refactor transcripts route to include channel name and export type parameters; implement response handling for text, CSV, and JSON formats

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
-from app.routes import transcripts as transcripts_router
+from app.routes.transcripts import router as transcripts_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
 
 app = FastAPI()
 
-app.add_api_route(transcripts_router, prefix="/api", tags=["transcripts"])
+# CORS middleware to allow requests from any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the YouTube Transcript API!"}
+
+app.include_router(transcripts_router, prefix="/api", tags=["transcripts"])
 
 if __name__ == "__main__":
     import uvicorn
--- app/routes/transcripts.py
+++ app/routes/transcripts.py
@@ -1,13 +1,49 @@
 from fastapi import APIRouter
+from fastapi import Query, Path, HTTPException
+from fastapi.responses import StreamingResponse
+from app.utils.writes import write_as_csv, write_as_text, write_as_json
+from app.fetch import fetch
 
 router = APIRouter()
 
-router.get("/fetch_transcripts")
-async def fetch_transcripts():
+@router.get("/transcripts/{channel_name}")
+async def fetch_transcripts(
+        channel_name : str = Path(description="Channel name to fetch transcripts from.", min_length=1, max_length=70),
+        export_type: str = Query(default="json", description="Export type for transcripts. Options: 'json', 'txt', 'csv'")
+        ):
     """
     Fetch transcripts for all videos from a channel.
     """
-    
-    pass
+    try:
+        if export_type not in ["json", "txt", "csv"]:
+            raise HTTPException(status_code=400, detail="Invalid export type. Options: 'json', 'txt', 'csv'")
+        
+        # Call the fetch function from fetch.py
+        transcripts = fetch(channel_name, export_type)
+
+        if export_type == "txt":
+            output = write_as_text(transcripts)
+
+            return StreamingResponse(output, media_type="text/plain", headers={
+                "Content-Disposition": "attachment; filename=transcripts.txt"
+            })
+        
+        elif export_type == 'csv':
+            output = write_as_csv(transcripts)
+
+            return StreamingResponse(output, media_type="text/csv", headers={
+                "Content-Disposition": "attachment; filename=transcripts.csv"
+            })
+        
+        elif export_type == 'json':
+            output = write_as_json(transcripts)
+
+            return StreamingResponse(output, media_type="application/json", headers={
+                "Content-Disposition": "attachment; filename=transcripts.json"
+            })
+
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
     
