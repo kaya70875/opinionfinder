7f4fc90707d917abb9ff2a207079df951f1800bd
Ahmet Kaya <shortsforturkey@gmail.com>
2025-05-21 22:08:45 +0300

    update fetch method

--- app/fetch.py
+++ app/fetch.py
@@ -1,47 +1,61 @@
-from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound, VideoUnavailable, TranscriptsDisabled
 import asyncio
-from threading import Lock
-from app.types.youtube import Snippet
-from app.types.youtube import FetchAndMetaResponse
+from concurrent.futures import ThreadPoolExecutor
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api._errors import NoTranscriptFound, VideoUnavailable, TranscriptsDisabled, IpBlocked
+from tenacity import retry, wait_fixed, retry_if_exception_type, stop_after_attempt
+from app.types.youtube import Snippet, FetchAndMetaResponse
+import requests
+import time
 
-ytt_api = YouTubeTranscriptApi()
-write_lock = Lock()
+class CustomHTTPClient(requests.Session):
+    def __init__(self, timeout=5):
+        super().__init__()
+        self.timeout = timeout
 
+    def get(self, url, **kwargs):
+        kwargs.setdefault("timeout", self.timeout)
+        return super().get(url, **kwargs)
+
+# Global API and thread pool
+executor = ThreadPoolExecutor(max_workers=30)
+
+@retry(
+    retry=retry_if_exception_type(IpBlocked),
+    wait=wait_fixed(1),
+    stop=stop_after_attempt(2)
+)
 def fetch_transcript_with_snippet(video_id: str, snippet: Snippet) -> dict | None:
     try:
-        print(f"Fetching transcript for {video_id}...")
+        ytt_api = YouTubeTranscriptApi()        
         transcript = ytt_api.fetch(video_id).to_raw_data()
-        print(f"✅ Transcript for {video_id} saved.")
+        print(f"✅ {video_id} done")
         return {
             "video_id": video_id,
             "transcript": transcript,
-            "snippet": {
-                "title": snippet.title,
-                "description": snippet.description,
-                "publishedAt": snippet.publishedAt,
-                "channelId": snippet.channelId
-            }
+            "snippet": snippet.model_dump()
         }
-    except (NoTranscriptFound, VideoUnavailable, TranscriptsDisabled) as e:
-        print(f"❌ Error for {video_id}: {e}")
+    except (NoTranscriptFound, VideoUnavailable, TranscriptsDisabled):
         return None
     except Exception as e:
-        print(f"⚠️ Error for {video_id}: {e}")
+        print(f"⚠️ Unexpected error: {e}")
         return None
 
+
 async def fetch_all_transcripts_with_metadata(video_ids: list[str], snippets: list[Snippet]) -> list[FetchAndMetaResponse]:
+    start = time.perf_counter()
     async def run_in_thread(vid, snip):
         return await asyncio.to_thread(fetch_transcript_with_snippet, vid, snip)
 
     tasks = [run_in_thread(vid, snip) for vid, snip in zip(video_ids, snippets)]
-    results_raw = await asyncio.gather(*tasks)
+    results = await asyncio.gather(*tasks)
+    end = time.perf_counter()
 
+    print(f"Took {end-start} seconds to fetch all transcripts.")
     return [
         FetchAndMetaResponse(
             video_id=result["video_id"],
             transcript=result["transcript"],
             snippet=Snippet(**result["snippet"])
         )
-        for result in results_raw if result
+        for result in results if result
     ]
-
