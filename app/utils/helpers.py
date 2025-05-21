a2b7a51c04ae00f9143781bca245b3af9df8e002
Ahmet Kaya <shortsforturkey@gmail.com>
2025-05-21 22:09:24 +0300

    refactor: streamline error handling and logging in get_channel_id function

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -2,52 +2,45 @@ import httpx
 from app.types.youtube import ChannelData
 import logging
 from app.lib.timeout import TIMEOUT
+from tenacity import retry, wait_fixed, stop_after_attempt, retry_if_exception_type
+import time
 
 logger = logging.getLogger(__name__)
-
-import httpx
-import traceback
-
+logging.basicConfig()
+@retry(
+        retry=retry_if_exception_type((httpx.ConnectTimeout, httpx.ReadTimeout)),
+        wait=wait_fixed(2),
+        stop=stop_after_attempt(3),
+        reraise=True
+)
 async def get_channel_id(channel_name: str, api_key: str) -> str:
-    try:
-        print(f"🌐 Preparing request for @{channel_name}")
-        async with httpx.AsyncClient(timeout=TIMEOUT) as client:
-            print(f"📤 Sending request...")
-            response = await client.get(
-                'https://www.googleapis.com/youtube/v3/channels',
-                params={
-                    'part': 'id',
-                    'forHandle': f'@{channel_name}',
-                    'key': api_key
-                }
-            )
-            print(f"📥 Response status: {response.status_code}")
-            response.raise_for_status()
+    #print(f"🌐 Preparing request for @{channel_name}")
+    start = time.perf_counter()
+    async with httpx.AsyncClient(timeout=TIMEOUT) as client:
+        print(f"📤 Sending request...")
+        response = await client.get(
+            'https://www.googleapis.com/youtube/v3/channels',
+            params={
+                'part': 'id',
+                'forHandle': f'@{channel_name}',
+                'key': api_key
+            }
+        )
+        response.raise_for_status()
+        res = response.json()
 
-            print(f"📄 Raw response: {response.text}")
-            res = response.json()
-
-            if 'items' in res and res['items']:
-                return res['items'][0]['id']
-            else:
-                raise ValueError(f"Channel '{channel_name}' not found.")
-    
-    except httpx.ConnectTimeout as e:
-        print("❌ Connect timeout:", e)
-    except httpx.ReadTimeout as e:
-        print("❌ Read timeout:", e)
-    except httpx.RequestError as e:
-        print("❌ Request error:", e)
-    except httpx.HTTPStatusError as e:
-        print(f"❌ HTTP status error: {e.response.status_code} - {e.response.text}")
-    except Exception as e:
-        print("❌ Unexpected error:", e)
-        traceback.print_exc()
+        if 'items' in res and res['items']:
+            end = time.perf_counter()
+            print(f"Took {end - start:.2f} seconds to get channel id")
+            return res['items'][0]['id']
+        else:
+            raise ValueError(f"Channel '{channel_name}' not found.")
 
 
 
 async def fetch_with_playlist_id(uploads_playlist_id, api_key: str, max_results: int) -> ChannelData:
     try:
+        start = time.perf_counter()
         data = {
             'video_ids' : [],
             'metadata' : []
@@ -74,13 +67,14 @@ async def fetch_with_playlist_id(uploads_playlist_id, api_key: str, max_results:
 
                     data['video_ids'].append(video_id)
                     data['metadata'].append(snippet)
-                    print(f"Fetched video ID: {video_id}")
 
                 next_page_token = res.get('nextPageToken')
                 if not next_page_token:
                     break
-            print('data', data)
-            return ChannelData(**data)
+        print('total videos fetched: ',len(data['video_ids']))
+        end = time.perf_counter()
+        print(f"Took {end - start:.2f} seconds for fetch_with_playlist_id")
+        return ChannelData(**data)
     except AttributeError as attr_err:
         logger.error('Error fetching vide IDs:',attr_err)
         print('Error fetching video IDs:', attr_err)
