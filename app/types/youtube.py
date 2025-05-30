32c7997ef806ec55399c63a88999106b02f25970
Ahmet Kaya <shortsforturkey@gmail.com>
2025-05-30 19:44:07 +0300

    add thumbnails to response

--- app/types/youtube.py
+++ app/types/youtube.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
+from app.types.thumbnails import Thumbnails
 
 class Snippet(BaseModel):
     title: str
     description: str
     publishedAt: str
     channelId: str
+    thumbnails: Thumbnails
     
 class ChannelData(BaseModel):
     video_ids: list[str]
