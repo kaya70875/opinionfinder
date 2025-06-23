c0e85cbad614ce26b92d6533eb7b0059fcad4a4d
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-23 18:06:14 +0300

    save job information to redis for reusing

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -4,7 +4,7 @@ from fastapi.responses import StreamingResponse
 from app.user.extract_jwt_token import get_user_id
 from arq.connections import RedisSettings
 from app.lib.rd import r
-from app.utils.jobs import get_user_jobs_from_redis
+from app.utils.jobs import get_user_jobs_from_redis, save_job_to_redis
 from pydantic import BaseModel
 from typing import List, Annotated
 from app.types.youtube import FetchAndMetaResponse
@@ -40,12 +40,12 @@ async def get_job_progress(progress_id: str):
     async def event_generator():
         last_progress = -1
         while True:
-            await asyncio.sleep(0.5)  # Poll Redis every half second (or pub/sub if you want real-time)
+            await asyncio.sleep(0.5)
             progress = int(r.get(f"progress:{progress_id}") or 0)
             if progress != last_progress:
                 yield f"data: {progress}\n\n"
                 last_progress = progress
-            if progress >= 100:  # Optional: when finished, break
+            if progress >= 100:
                 break
     return StreamingResponse(event_generator(), media_type="text/event-stream")
 
@@ -55,21 +55,13 @@ async def get_job_status(job_id: str, user_id: Annotated[str, Depends(get_user_i
     job = Job(job_id=job_id, redis=redis)
 
     # Get necessarry fields from redis
-    # queries = r.hgetall(f'query:{job_id}')
+    queries = r.hgetall(f'query:{job_id}')
 
     # Get job results
     job_results = await job.result()
     results: list[FetchAndMetaResponse] = [FetchAndMetaResponse.model_validate(result) for result in job_results.get("data")]
 
     # Save job informations to database 
-    # save_job_to_redis(user_id, job.job_id, queries['channel_name'], queries['max_results'], results)
-
-    # Set transcript results to redis for getting in download route without fetching the results again.
-    r.set(f'transcript:{job_id}', json.dumps([result.model_dump() for result in results]))
-    r.expire(f'transcript:{job_id}', 60 * 60 * 2)
-
-    # Track the job under the user key using a Redis set
-    r.sadd(f"user:{user_id}:jobs", job_id)
-    r.expire(f"user:{user_id}:jobs", 60 * 60)
+    save_job_to_redis(user_id, job.job_id, queries['channel_name'], queries['max_results'], results)
 
     return {"data" : results}
