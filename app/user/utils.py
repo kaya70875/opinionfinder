6d778db831b4f65c0ad1a85c9be9b7abc28e4d4c
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-24 14:26:34 +0300

    cache user plan with redis

--- app/user/utils.py
+++ app/user/utils.py
@@ -1,5 +1,6 @@
 from app.lib.database import db
 from bson import ObjectId
+from app.lib.rd import r
 
 async def get_user_plan(user_id: str) -> str:
     """
@@ -7,9 +8,17 @@ async def get_user_plan(user_id: str) -> str:
     """
 
     try:
+        user_plan_key = f"user:{user_id}:plan"
+        cached_plan = r.get(user_plan_key)
+        # First try to get user plan from cache.
+        if cached_plan is not None:
+            return cached_plan
+
         user = await db['users'].find_one({"_id": ObjectId(user_id)})
+        user_plan = user.get("plan", "free")
         if user:
-            return user.get("plan", "free")
+            r.set(user_plan_key, user_plan, 60 * 60 * 2)
+            return user_plan
         else:
             raise ValueError(f"User with ID {user_id} not found.")
     except Exception as e:
