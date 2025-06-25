54e6610b3d9b8db29ce72016d2fe785c298e6447
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-25 20:24:06 +0300

    remove unused utilty function

--- app/utils/jobs.py
+++ app/utils/jobs.py
@@ -1,36 +1,13 @@
 from app.lib.database import db
 from app.types.youtube import FetchAndMetaResponse
 from typing import List
-from bson import ObjectId
 from app.lib.rd import r
 from fastapi import HTTPException
-import gzip
 import json
 
 collection = db.get_collection("jobs")
 
 REDIS_EXPIRY = 60 * 60 * 2
-
-async def save_job(user_id: str, channel_name: str, job_id: str, max_results: int, results: List[FetchAndMetaResponse]):
-    """
-    Saves job informations to MongoDB database.
-    """
-
-    try:
-        # Check if there is same records or not.
-
-        # Compress results for reducing json size for optimization.
-        compressed = gzip.compress(json.dumps([result.model_dump() for result in results]).encode())
-
-        await collection.insert_one({
-            "userId": ObjectId(user_id),
-            "jobId": job_id,
-            "channelName": channel_name,
-            "totalFetched": max_results,
-            "results": compressed
-        })
-    except Exception as e:
-        print('Error while inserting job information to db : ', e)
 
 def save_job_to_redis(user_id: str, job_id: str, queries: dict, results: List[FetchAndMetaResponse]):
     """
