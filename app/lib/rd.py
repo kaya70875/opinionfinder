63f4ad692ae461eb7fec5d8aff2da344ad7dace7
Ahmet Kaya <shortsforturkey@gmail.com>
2025-06-28 15:59:23 +0300

    feat: refactor Redis configuration to use environment variables for host, port, and password

--- app/lib/rd.py
+++ app/lib/rd.py
@@ -1,15 +1,18 @@
-import redis
-import os
+from redis import Redis
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
-host = os.getenv('REDIS_HOST')
-password = os.getenv('REDIS_PASSWORD')
+ENVIRONMENT = os.getenv("ENVIRONMENT", "localhost")
 
-r = redis.Redis(
-    host=host,
-    port=15382,
-    decode_responses=True,
-    username="default",
-    password=password,
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+REDIS_PORT = 15382
+
+r = Redis(
+    host=REDIS_HOST if ENVIRONMENT == 'prod' else 'localhost',
+    port=REDIS_PORT if ENVIRONMENT == 'prod' else 6379,
+    password=REDIS_PASSWORD if ENVIRONMENT == 'prod' else None,
+    username="default" if ENVIRONMENT == 'prod' else None,
+    decode_responses=True
 )
--- app/lib/redis_settings.py
+++ app/lib/redis_settings.py
@@ -3,13 +3,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+ENVIRONMENT = os.getenv("ENVIRONMENT", 'localhost')
+
 REDIS_HOST = os.getenv('REDIS_HOST')
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+REDIS_PORT = 15382
 
 REDIS_CONF = RedisSettings(
-    host=REDIS_HOST,
-    port=15382,
-    password=REDIS_PASSWORD,
-    username="default",
+    host=REDIS_HOST if ENVIRONMENT == 'prod' else 'localhost',
+    port=REDIS_PORT if ENVIRONMENT == 'prod' else 6379,
+    password=REDIS_PASSWORD if ENVIRONMENT == 'prod' else None,
+    username="default" if ENVIRONMENT == 'prod' else None,
     conn_timeout=60
 )
